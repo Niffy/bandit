9520338f4f51c7de4b3cce04a1e05609d73b300f
Paul Robinson <niffydroid@gmail.com>
2023-04-30 21:39:15 +0100

    Testing for rule adjacent character

--- src/__tests__/rules.test.js
+++ src/__tests__/rules.test.js
@@ -8,7 +8,28 @@ import {
 
 import { reelOptions } from '../reel'
 
+const slotCount = 4
+function allDifferent () {
+  const slots = []
+  for (let index = 0; index < slotCount; index++) {
+    slots.push(reelOptions[index])
+  }
+  return slots
+}
+
+function allTheSame () {
+  const slots = []
+  for (let index = 0; index < slotCount; index++) {
+    slots.push(reelOptions[2])
+  }
+  return slots
+}
+
 describe('Same Characters', () => {
+  let slots = []
+  beforeEach(() => {
+    slots = allTheSame()
+  })
   it('Should be defined', () => {
     expect(hasSameCharacters).toBeDefined()
   })
@@ -27,21 +48,11 @@ describe('Same Characters', () => {
   })
 
   it('Should return true when all slots are the same', () => {
-    const slots = []
-    const slotCount = 4
-    for (let index = 0; index < slotCount; index++) {
-      slots.push(reelOptions[0])
-    }
     const result = hasSameCharacters(slots)
     expect(result).toEqual(true)
   })
 
   it('Should return false when 1 slot is different', () => {
-    const slots = []
-    const slotCount = 4
-    for (let index = 0; index < slotCount; index++) {
-      slots.push(reelOptions[0])
-    }
     slots[0] = reelOptions[1]
     const result = hasSameCharacters(slots)
     expect(result).toEqual(false)
@@ -49,6 +60,11 @@ describe('Same Characters', () => {
 })
 
 describe('All different Characters', () => {
+  let slots = []
+  beforeEach(() => {
+    slots = allDifferent()
+  })
+
   it('Should be defined', () => {
     expect(hasDifferentCharacters).toBeDefined()
   })
@@ -67,22 +83,60 @@ describe('All different Characters', () => {
   })
 
   it('Should return false when all slots are the same', () => {
-    const slots = []
-    const slotCount = 4
-    for (let index = 0; index < slotCount; index++) {
-      slots.push(reelOptions[0])
-    }
+    const slots = allTheSame()
     const result = hasDifferentCharacters(slots)
     expect(result).toEqual(false)
   })
 
   it('Should return true when all slots are different', () => {
-    const slots = []
-    const slotCount = 4
-    for (let index = 0; index < slotCount; index++) {
-      slots.push(reelOptions[index])
-    }
     const result = hasDifferentCharacters(slots)
+    expect(result).toEqual(true)
+  })
+})
+
+describe('Adjacent Characters', () => {
+  let slots = []
+  beforeEach(() => {
+    slots = allDifferent()
+  })
+  it('Should be defined', () => {
+    expect(adjacentCharacters).toBeDefined()
+  })
+
+  it('Should have a string result defined', () => {
+    expect(rules.ADJACENT_CHARACTERS.result).toBeDefined()
+  })
+
+  it('Should have a function that can be called', () => {
+    expect(rules.ADJACENT_CHARACTERS.fnc).toBeDefined()
+    rules.ADJACENT_CHARACTERS.fnc([])
+  })
+
+  it('Should have the correct function', () => {
+    expect(rules.ADJACENT_CHARACTERS.fnc).toEqual(adjacentCharacters)
+  })
+
+  it('Should return true when all slots are the same', () => {
+    slots = allTheSame()
+    const result = adjacentCharacters(slots)
+    expect(result).toEqual(true)
+  })
+
+  it('Should return true when slots 1 and 2 are the same', () => {
+    slots[0] = slots[1] = reelOptions[0]
+    const result = adjacentCharacters(slots)
+    expect(result).toEqual(true)
+  })
+
+  it('Should return true when slots 2 and 3 are the same', () => {
+    slots[1] = slots[2] = reelOptions[0]
+    const result = adjacentCharacters(slots)
+    expect(result).toEqual(true)
+  })
+
+  it('Should return true when slots 3 and 4 are the same', () => {
+    slots[2] = slots[3] = reelOptions[0]
+    const result = adjacentCharacters(slots)
     expect(result).toEqual(true)
   })
 })
