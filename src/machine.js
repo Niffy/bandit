5875dca58c4d85033444f0853322b22f1f405b8b
Paul Robinson <niffydroid@gmail.com>
2023-05-01 15:46:10 +0100

    Some logic for the machine itself

--- src/machine.js
+++ src/machine.js
@@ -1,9 +1,18 @@
-import { costPerPlay, rules, checkSlots } from './rules.js'
+import { checkSlots } from './rules.js'
+import { constants, costPerPlay } from './constants.js'
 import { getRandomSlots } from './random.js'
 
+/**
+ * This class handles the slot machine process. i.e tie everything together
+ *
+ */
 export class Machine {
-  constructor (startingPot) {
-    this.userPot = startingPot
+  /**
+   * Create an instance of the machine
+   * @param {Number} startingBalance The starting balance of the user, using GBP pennies eg 20 = 20p, 200 - £2
+   */
+  constructor (startingBalance) {
+    this.userBalance = startingBalance
     this.balance = 2000
   }
 
@@ -11,8 +20,54 @@ export class Machine {
 
   }
 
+  /**
+   * Handle subtracting the cost of play from the users
+   * @returns {Boolean} if the subtraction worked
+   */
+  userSubtract () {
+    if (this.userBalance < costPerPlay) {
+      return false
+    }
+    this.userBalance -= costPerPlay
+    return true
+  }
+
+  /**
+   * Check the user can do a spin by checking the user balance
+   * @returns {Boolean} True if they have enoug credit, false if not
+   */
+  canExecute () {
+    if (this.userBalance === 0 || this.userBalance < costPerPlay) {
+      return false
+    }
+    return true
+  }
+
+  /**
+   * Get the user balance
+   * @returns {Number} The user balance using GBP pennies eg 20 = 20p, 200 - £2
+   */
+  getUserBalance () {
+    return this.userBalance
+  }
+
+  /**
+   * Spin the machine
+   * @returns {Object}
+   */
   execute () {
-    const slots = getRandomSlots()
-    const result = checkSlots(slots)
+    // Determine the user has enough credit to play
+    if (this.canExecute()) {
+      // Go and do the subtraction from the user balance
+      this.userSubtract()
+      // Get the random slots
+      const slots = getRandomSlots()
+      // Check the result
+      const ruleResult = checkSlots(slots)
+      // Go and handle the result and do any extra processing such as awarding money or increasing balance
+      const result = this.handleResult(ruleResult)
+    } else {
+      return 'NO_BALANCE'
+    }
   }
 }
